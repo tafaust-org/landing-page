0d83b51030a0f3630846feaadb19a79fbf71ca75
Thomas Hesse <thomas.hesse@neoloan.de>
2022-08-02 19:33:34 +0200

    feat: add preliminary draft for a new landing page
    for review

---
+++ src/hero/HeroOneButton.tsx
@@ -0,0 +1,22 @@
+import { ReactNode } from 'react';
+import {Button} from "../button/Button";
+
+type IHeroOneButtonProps = {
+  title: ReactNode;
+  description: string;
+  button: ReactNode;
+};
+
+const HeroOneButton = (props: IHeroOneButtonProps) => (
+  <header className="text-center">
+    <h1 className="text-5xl text-gray-900 font-bold whitespace-pre-line leading-hero">
+      {props.title}
+    </h1>
+    <div className="text-2xl mt-4 mb-16 mix-blend-difference">{props.description}</div>
+
+    {/*{props.button}*/}
+    <input type={'text'} className={'shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'} placeholder={'your@email.com'} /> <Button>Learn more</Button>
+  </header>
+);
+
+export { HeroOneButton };
---
+++ src/templates/Hero.tsx
@@ -0,0 +1,49 @@
+import Link from 'next/link';
+
+import { Background } from '../background/Background';
+import { HeroOneButton } from '../hero/HeroOneButton';
+import { Section } from '../layout/Section';
+import { NavbarTwoColumns } from '../navigation/NavbarTwoColumns';
+import { Logo } from './Logo';
+
+const Hero = () => (
+  <Background color={"wave bg-gradient-to-r from-red-200 via-red-500 to-yellow-400 background-animate"}
+  >
+    <Section yPadding="py-6">
+      <NavbarTwoColumns logo={<Logo xl />}>
+        <li>
+          <Link href="#features">
+            <a className={`hover:bg-white p-3 rounded-md`}>Features</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="#contact">
+            <a className={`hover:bg-white p-3 rounded-md`}>Contact</a>
+          </Link>
+        </li>
+      </NavbarTwoColumns>
+    </Section>
+
+    <Section yPadding="pt-20 pb-32">
+      <HeroOneButton
+        title={
+          <>
+            {'A smarter construction site with\n'}
+            <span>Buildin</span><span className="text-primary-500">X</span>
+          </>
+        }
+        description="We enable direct human-construction component interaction to foster efficiency and embrace data ownership in BIM digital twins."
+        button={''}
+        // button={
+        //   <Link href="https://creativedesignsguru.com/category/nextjs/">
+        //     <a>
+        //       <Button xl>Download Your Free Theme</Button>
+        //     </a>
+        //   </Link>
+        // }
+      />
+    </Section>
+  </Background>
+);
+
+export { Hero };
